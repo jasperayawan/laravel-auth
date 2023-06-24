6da626bbf1d161ba336d0b7b239973be2d8d5b55
jasperayawan <ejayawan22@gmail.com>
2023-06-24 22:36:34 +0800

    update registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class UserController extends Controller
 {
@@ -24,5 +27,32 @@ class UserController extends Controller
 
     public function saveUser(Request $request){
         //validate all the fields
+        $validator = FacadesValidator::make($request->all(),[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:100', //users is a table
+            'password' => 'required|min:6|max:50',
+            'cpassword' => 'required|min:6|same:password'
+        ],[
+            //define some custom message
+            'cpassword.same' => 'Password did not matched!',
+            'cpassword.required' => 'Confirm password is required!',
+        ]); 
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 400,
+                'messages' => $validator->getMessageBag()
+            ]);
+        } else {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+            return response()->json([
+                'status' => 200,
+                'messages' => 'Registered successfully!'
+            ]);
+        }
     }
 }
